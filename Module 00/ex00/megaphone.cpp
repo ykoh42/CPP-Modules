0d3488000f6204ce38d7ff7013eceeb7418d92fc
Ykoh <ykoh@student.42seoul.kr>
2021-03-18 02:59:52 +0900

    Fix : ex00

--- Module 00/ex00/megaphone.cpp
+++ Module 00/ex00/megaphone.cpp
@@ -2,22 +2,23 @@
 
 int main(int argc, char *argv[])
 {
-    std::locale loc;
 
-    if (argc > 1)
+    if (argc == 1)
     {
-        for (int i = 1; i < argc; i++)
-        {
-            std::string str = argv[i];
-            for(std::string::size_type j = 0; j < str.length(); j++)
-            {
-                std::cout << std::toupper(str[j],loc);
-            }
-        }
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     }
     else
     {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        std::locale loc;
+        
+        for (int i = 1; i < argc; i++)
+        {
+            std::string str(argv[i]);
+            for(std::string::size_type j = 0; j < str.length(); j++)
+            {
+                std::cout << std::toupper(str[j], loc);
+            }
+        }
     }
     std::cout << std::endl;
     return (0);
