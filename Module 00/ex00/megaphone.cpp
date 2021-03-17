2743c927815b5a37aba1289fef0c490995945eea
Ykoh <ykoh@student.42seoul.kr>
2021-03-18 03:00:32 +0900

    Fix : typo

--- Module 00/ex00/megaphone.cpp
+++ Module 00/ex00/megaphone.cpp
@@ -10,10 +10,11 @@ int main(int argc, char *argv[])
     else
     {
         std::locale loc;
-        
+
         for (int i = 1; i < argc; i++)
         {
             std::string str(argv[i]);
+            
             for(std::string::size_type j = 0; j < str.length(); j++)
             {
                 std::cout << std::toupper(str[j], loc);
