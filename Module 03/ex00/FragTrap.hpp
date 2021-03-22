e90d43c5212417d9bab151e9fafa67876b4cfbf4
ykoh <ykoh@student.42seoul.kr>
2021-03-23 06:15:50 +0900

    Add: ex00

---
+++ Module 03/ex00/FragTrap.hpp
@@ -0,0 +1,36 @@
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+# include <iostream>
+
+class FragTrap
+{
+public:
+	FragTrap(const std::string& name);
+	FragTrap(const FragTrap& other);
+	~FragTrap(void);
+
+	FragTrap&	        operator=(const FragTrap& other);
+    void                rangedAttack(std::string const & target);
+    void                meleeAttack(std::string const & target);
+    void                takeDamage(unsigned int amount);
+    void                beRepaired(unsigned int amount);
+    void                vaulthunter_dot_exe(std::string const & target);
+
+private:
+    std::string         mName;
+    int                 mHitPoints;
+    int                 mEnergyPoints;
+    const int           mMaxHitPoints;
+    const int           mMaxEnergyPoints;
+    const int           mLevel;
+    const int           mMeleeAttackDamage;
+    const int           mRangedAttackDamage;
+    const int           mArmorDamageReduction;
+    static std::string  mAttackList[5];
+
+    const int&          getHitPoints(void) const;
+};
+
+
+#endif
