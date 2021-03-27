808e0c0d14b8ed9121d7f4ebbf2e69022a52b42a
ykoh <ykoh@student.42seoul.kr>
2021-03-27 09:33:56 +0900

    Add: function tests

--- Module 03/ex02/main.cpp
+++ Module 03/ex02/main.cpp
@@ -12,13 +12,17 @@ int main(void)
 
     std::cout << "------------function test-----------" << std::endl;
     std::cout << "HP : " << b.getHitPoints() << std::endl;
-    a.meleeAttack("b");
+    a.meleeAttack("BI");
     b.takeDamage(30);
+    std::cout << "HP : " <<  b.getHitPoints() << std::endl;
     std::cout << "------------------------------------" << std::endl;
     std::cout << "HP : " <<  a.getHitPoints() << std::endl;
-    b.meleeAttack("a");
+    b.meleeAttack("AI");
     a.takeDamage(20);
     std::cout << "HP : " <<  a.getHitPoints() << std::endl;
     std::cout << "------------------------------------" << std::endl;
+    a.vaulthunter_dot_exe("BI");
+    b.challengeNewcomer("AI");
+    std::cout << "----------destructor test-----------" << std::endl;
     return (0);
 }
