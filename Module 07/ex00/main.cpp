351d0a329a33d1f171bb87df2cc0feffe78e022a
ykoh <ykoh@student.42seoul.kr>
2021-04-03 09:44:51 +0900

    Add : ex00

---
+++ Module 07/ex00/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "whatever.hpp"
+
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+
+    std::cout << std::string(60, '-') << std::endl;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << std::string(60, '-') << std::endl;
+
+	std::cout << "min(a, b) = " << min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << max(a, b) << std::endl;
+    std::cout << std::string(60, '-') << std::endl;
+
+	std::string c = "appae";
+	std::string d = "appx3";
+
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << std::string(60, '-') << std::endl;
+
+	std::cout << "min(c, d) = " << min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << max(c, d) << std::endl;
+    std::cout << std::string(60, '-') << std::endl;
+	return (0);
+}
