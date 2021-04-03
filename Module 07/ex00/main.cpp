52d4e07a1b730cc96c9baa69317c9219b2d6b91a
ykoh <ykoh@student.42seoul.kr>
2021-04-03 09:47:05 +0900

    Fix : typo

--- Module 07/ex00/main.cpp
+++ Module 07/ex00/main.cpp
@@ -8,24 +8,24 @@ int main(void)
 
     std::cout << std::string(60, '-') << std::endl;
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	swap(a, b);
+	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << std::string(60, '-') << std::endl;
 
-	std::cout << "min(a, b) = " << min(a, b) << std::endl;
-	std::cout << "max(a, b) = " << max(a, b) << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
     std::cout << std::string(60, '-') << std::endl;
 
-	std::string c = "appae";
-	std::string d = "appx3";
+	std::string c = "chaine1";
+	std::string d = "chaine2";
 
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	swap(c, d);
+	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << std::string(60, '-') << std::endl;
 
-	std::cout << "min(c, d) = " << min(c, d) << std::endl;
-	std::cout << "max(c, d) = " << max(c, d) << std::endl;
+	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
     std::cout << std::string(60, '-') << std::endl;
 	return (0);
 }
