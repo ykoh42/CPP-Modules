42f84bd7d52fee5c7a59288252d88dfad6418a73
ykoh <ykoh@student.42seoul.kr>
2021-04-04 22:25:48 +0900

    Fix : iter overloading

--- Module 07/ex01/iter.hpp
+++ Module 07/ex01/iter.hpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 template<typename T>
+void    iter(T* arr, std::size_t length, void (*f)(T& elem))
+{
+	for (std::size_t i = 0; i < length; i++)
+    {
+		(*f)(arr[i]);
+    }
+}
+
+template<typename T>
 void    iter(T* arr, std::size_t length, void (*f)(const T& elem))
 {
 	for (std::size_t i = 0; i < length; i++)
