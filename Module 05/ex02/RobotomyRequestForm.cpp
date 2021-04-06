bca9f73988bda973fcf3c3a2175751f3efee0f22
ykoh <ykoh@student.42seoul.kr>
2021-04-06 17:16:42 +0900

    Fix : execute function ramdomly

--- Module 05/ex02/RobotomyRequestForm.cpp
+++ Module 05/ex02/RobotomyRequestForm.cpp
@@ -4,16 +4,19 @@ RobotomyRequestForm::RobotomyRequestForm(void)
     : Form("RobotomyRequest", 72, 45)
     , mTarget("undefined")
 {
+    srand(time(NULL));
 }
 
 RobotomyRequestForm::RobotomyRequestForm(const std::string& target)
     : Form("RobotomyRequest", 72, 45)
     , mTarget(target)
 {
+    srand(time(NULL));
 }
 
 RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& other)
 {
+    srand(time(NULL));
     *this = other;
 }
 
@@ -34,7 +37,14 @@ void                    RobotomyRequestForm::execute(const Bureaucrat& bureaucra
         if (bureaucrat.GetGrade() <= Form::GetExecuteGrade())
         {
             std::cout << "Ziiigg...." << std::endl;
-            std::cout << "<" << mTarget << "> has been robotomized successfully 50% of the time." << std::endl;
+            if (rand() % 2)
+            {
+                std::cout << "<" << mTarget << "> has been robotomized successfully." << std::endl;
+            }
+            else
+            {
+                std::cout << "<" << mTarget << "> robotomization has failed." << std::endl;
+            }
         }
         else
         {
